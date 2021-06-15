3e611efb09a65e6b886fb518f5d447cefbeac372
squatcub <brandon.alexis10@hotmail.com>
2021-06-14 23:44:23 -0500

    Pods para carrusel y firebase instalados

--- Naturapp/ViewController.swift
+++ Naturapp/ViewController.swift
@@ -6,12 +6,33 @@
 //
 
 import UIKit
+import iCarousel
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, iCarouselDataSource {
+    
+    //Inicializacion del carrusel
+    let myCarousel: iCarousel = {
+        let view = iCarousel()
+        view.type = .coverFlow
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //Se agrega el carrusel a la vista principal
+        view.addSubview(myCarousel)
+        myCarousel.dataSource = self
+        myCarousel.frame = CGRect(x: 0, y: 20, width: view.frame.size.width, height: 100)
+    }
+    
+    //Metodos para el carrusel
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return 10
+    }
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 250, height: 100))
+        view.backgroundColor = .blue
+        return view
     }
 
 
---
+++ Pods/Target Support Files/Pods-Naturapp/Pods-Naturapp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Naturapp : NSObject
+@end
+@implementation PodsDummy_Pods_Naturapp
+@end
