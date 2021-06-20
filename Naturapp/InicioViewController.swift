d80bef5c968b2aa6b9f6063836b7418bf80804f3
squatcub <brandon.alexis10@hotmail.com>
2021-06-19 19:15:39 -0500

    Primera implementacion del collectionview con celda personalizada

--- Naturapp/InicioViewController.swift
+++ Naturapp/InicioViewController.swift
@@ -10,12 +10,18 @@ import Firebase
 
 class InicioViewController: UIViewController {
     //Outlets del storyboard
+    @IBOutlet weak var collectionView: UICollectionView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        initialSetuo()
+        let collectionLayout = UICollectionViewFlowLayout()
+        collectionLayout.itemSize = CGSize(width: view.frame.size.width/2 - 20, height: 150)
+        collectionView.collectionViewLayout = collectionLayout
+        collectionView.register(UINib(nibName: "MyCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "col")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        initialSetup()
         // Do any additional setup after loading the view.
     }
 
@@ -30,8 +36,33 @@ class InicioViewController: UIViewController {
         }
     }
     
-    func initialSetuo() {
+    func initialSetup() {
         navigationController?.isNavigationBarHidden = false
         navigationItem.hidesBackButton = true
+    }
+}
+
+extension InicioViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        print("Clicked here!")
+    }
+}
+
+extension InicioViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "col", for: indexPath) as! MyCollectionViewCell
+        cell.imageView.image = UIImage(named: "img1")
+        cell.tituloLabel.text = "Hola"
+        return cell
+    }
+}
+
+extension InicioViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.size.width/2 - 20, height: 150)
     }
 }
---
+++ Naturapp/MyCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MyCollectionViewCell.swift
+//  Naturapp
+//
+//  Created by Brandon Rodriguez Molina on 19/06/21.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var tituloLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        imageView.contentMode = .scaleToFill
+    }
+
+}
